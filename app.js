cbbe7350fbf4c105a1fc7ce71dfa6216db5f5917
Mustafa Ali <mustafaaaly27@gmail.com>
2024-07-04 15:22:37 +0500

    spread operator ( three dots )

--- app.js
+++ app.js
@@ -135,6 +135,145 @@
 // console.log(singleLineReturn);
 
 
+// -----------------------------------X 
+
+
+// Creating an object inside an arrow function -------------------- IMPORTANT --------
+
+// const createPerson = (name , age , profession) => {
+
+//     const identity = {
+//         name : name ,
+//         age : age ,
+//         profession : profession,
+//         greet : () =>{
+//             console.log(`This is ${name} & my age is ${age} years old`);
+//         }
+//     }
+//     return identity
+// }
+
+// const person1 = createPerson("mustafa" , 20 , "web developer")
+// // console.log(person1);
+// // console.log(person1.greet());
+// person1.greet()
+
+// createPerson("Mustafa" , 20 ,"web").greet() // this will invoke the greet method inside an object inside a function 
+
+
+// ---------------X------ARRAY DESTRUCTURING------------------------X----------------------X 
+
+// what is that? 
+
+// imagine an array with multiple values 
+// const arr = ["Pakistan" , "England" , "portugal" , "argentina"]
+
+// so array destructuring is get array values in an variable 
+// SYNTAX 
+// const [country2,country1] = arr // this will make a variable and assigned the values in array at that index 
+
+//it works respectively means phla variable phli value lega array ki  second variable means second value and so on
+
+// console.log(country1);
+// console.log(country2);
+
+// ----------------------------X _-----------------------------------------X ------------------X
+
+
+// ---------------------------X ------NOW OBJECT DESTRUCTURING--------------------------------------X
+// To get object properties inside a variable 
+// note that it makes global variable with values we inside from object
+
+
+// SYNTAX ?
+
+// Created an Object
+
+// const obj = {
+// country : "Argetina",
+// club : "inter miami",
+// name : "Messi",
+// Number : 10
+// }
+
+// We only need country and name from that object inside variable  
+
+// NOTED( property and variable name must be same in order to do that)
+
+// const{country , name , Number} = obj
+
+// console.log("name" , name );
+// console.log("Country" ,country);
+// console.log("Number" , Number);
+
+
+
+// ----------------------------------------X--------------------------------------------------------X 
+
+// backtick method i.e `` this is called ternary operator  
+// it will allow u to concede and add variable and values and allows line break aswell
+// For e.g 
+
+
+// let firstName = "Mustafa"
+// let lastName = "ali"
+// let greet = `Hi my name is ${firstName} ${lastName}`
+// console.log(greet);
+
+
+// --------------------------------------X ------------------------------------X 
+
+
+
+
+// How to concede to arrays? 
+// in Basic Javascript we do it like that 
+// let arr1 = ["Mustafa"]
+// let arr2 = [ "Ali"]
+
+// let arr = arr1.concat(arr2)
+// console.log(arr);
+
+// --BUT IN ADVANCE JAVASCRIPT WE USE SPREAD OPERATOR 
+// SPREAD OPERATOR (3 dots operator )
+// It concat Arrays and objects 
+
+
+// FOR ARRAYS 
+
+
+// let arr1 = ["Arr1 Value"]
+// let arr2 = ["Arr 2 value"]
+// let arr3 = [...arr1 , ...arr2]
+// console.log(arr3);
+
+// It is also known as three dots 
+
+
+// FOR OBJECTS 
+
+// const obj1 = {
+//     name :  "MUSTAFA"
+// }
+// const obj2 = {
+//     age : 20
+// }
+
+// const obj3 = {...obj1,...obj2}
+// console.log(obj3);
+
+
+// // now we can access obj1 and obj2 properties in obj3 
+// console.log(obj3.name);
+
+
+// ------------------------------------X ---------------------------------------- X---------------------X 
+
+
+
+
+
+
 
 
 
